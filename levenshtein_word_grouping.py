9c6c4da988e7944df8f16fc75909103cce2d57c2
bogdan <bogdan.gusuleac@cybertech.ro>
2022-11-20 01:02:57 +0200

    Levensthein applied to the db

--- levenshtein_word_grouping.py
+++ levenshtein_word_grouping.py
@@ -1,21 +1,35 @@
 import json
+from Levenshtein import distance as lev
 
 from sklearn.feature_extraction.text import CountVectorizer
 
 from settings import *
 import re
 
+products = []
 tags_referencing_products = []
 with open(PRODUCTS_FILE, 'r') as product_file:
     products = json.load(product_file)
     # obtaining id from link
 
     # pre process the tags to obtain just the final words
-    tags_referencing_products = [{product['product_url']: [tag for tag in product['tags'] if len(tag) > 1 and re.match(r'[a-zA-Z]+', tag)]} for product in products]
+    tags_referencing_products = [set([tag for tag in product['tags'] if len(tag) > 1 and re.match(r'[a-zA-Z]+', tag)]) for product in products]
 
+prompt_data = input('Enter prompt')
+prompt_data = set(prompt_data.split(" "))
 
-print(tags_referencing_products)
+tags_referencing_products = [list(tags) for tags in tags_referencing_products if prompt_data.intersection(tags)]
 
+minimal_distances = []
+for i in tags_referencing_products:
+    for j in tags_referencing_products:
+        if i != j:
+            minimal_distances.append((lev(i, j), i, j))
+
+minimal_distances = sorted(minimal_distances, key=lambda x: x[0])[:20]
+
+for elem in minimal_distances:
+    print(elem)
 
 # make a TFIDF transformer for the given elements
 # cv = CountVectorizer(max_df=0.85, max_features=len(tags_referencing_products))
